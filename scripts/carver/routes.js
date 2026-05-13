c3c1f121d82a5fc5afde9bd3b4d8e5b1e7f56fc3
Tianchi <imtiazsumaliman383@gmail.com>
2026-05-13 16:08:37 +0100

    started the router logic

--- scripts/carver/routes.js
+++ scripts/carver/routes.js
@@ -1,13 +1,63 @@
-import {} from 'controller';
+import {} from "controller";
 
 export class router {
-  constructor(route) {
-    this.route = route
+  constructor(
+    getRoutes = {},
+    postRoutes = {},
+    putRoutes = {},
+    deleteRoutes = {},
+  ) {
+    this.getRoutes = getRoutes;
+    this.postRoutes = postRoutes;
+    this.putRoutes = putRoutes;
+    this.deleteRoutes = deleteRoutes;
   }
-  
-  if (this.route === 'unmutable/tools') {
-    this.get(type, id = null) {
-      
-    }
+
+  get(route, options = {}) {
+    const controller = this.getRoutes[route];
+    if (!controller) throw new Error(`GET route not found: ${route}`);
+
+    const req = {
+      params: options.params || null,
+      body: options.body || null,
+    };
+
+    return controller(req);
+  }
+
+  post(route, options = {}) {
+    const controller = this.postRoutes[route];
+    if (!controller) throw new Error(`POST route not found: ${route}`);
+
+    const req = {
+      params: options.params || null,
+      body: options.body || null,
+    };
+
+    return controller(req);
+  }
+
+  put(route, options = {}) {
+    const controller = this.putRoutes[route];
+    if (!controller) throw new Error(`PUT route not found: ${route}`);
+
+    const req = {
+      params: options.params || null,
+      body: options.body || null,
+    };
+
+    return controller(req);
+  }
+
+  delete(route, options = {}) {
+    const controller = this.deleteRoutes[route];
+    if (!controller) throw new Error(`DELETE route not found: ${route}`);
+
+    const req = {
+      params: options.params || null,
+      body: options.body || null,
+    };
+
+    return controller(req);
   }
 }
